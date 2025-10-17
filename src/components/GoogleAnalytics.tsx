0ce554dac51034410955015739660d4e13e8557f
Yeshwanth-King <yeshuyeshwanth2005@gmail.com>
2025-10-17 13:44:41 +0530

    gtag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,7 +2,8 @@
 
 import Script from "next/script";
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_MEASUREMENT_ID =
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-R904QES3WC";
 
 export default function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) {
